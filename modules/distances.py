9456105dc383f60d14c96c574f1d4496d28ab6bb
Hamza LACHQAR <lachquarhamza@gmail.com>
2021-06-29 12:44:33 +0100

    backup

--- modules/distances.py
+++ modules/distances.py
@@ -1,7 +1,10 @@
+def have_same_size(img1, img2):
+    pass
+
 def euclidean(img1, img2):
     if(have_same_size(img1, img2)):
         distance = 0
-        distance = numpy.sum((img1 - img2) ** 2)
+        distance = numpy.sum(numpy.abs((img1 - img2)) ** 2)
         return numpy.sqrt(distance)
     else:
         return -1
